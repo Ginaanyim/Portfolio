3e010cb17acbdd50ad603fa2fae083c25874910d
Virginia Anyim <ginaanyim@hotmail.com>
2025-12-28 00:44:11 +0100

    Add email validation with colored text

--- js/script.js
+++ js/script.js
@@ -10,13 +10,32 @@ messageInput.addEventListener('input', function() {
 
     //Mäter antal tecken i meddelandefältet
     const currentLength = messageInput.value.length;
-
     charCount.textContent = currentLength + " / 20 characters";
 
+    //Ändrar färg på räcknaren baserat på antal tecken
     if (currentLength >= 20) {
         charCount.style.color = "green";
     } else {
         charCount.style.color = 'black';
     }
+});
+
+contactForm.addEventListener('submit', function(event){
+
+    const emailValue = emailInput.value;
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailPattern.test(emailValue)) {
+        event.preventDefault();
+        emailError.textContent = "Please enter a valid email address.";
+
+        emailError.style.display = "block";
+        
+        emailInput.style.borderColor = "red";
+    } else {
+        emailError.style.display = "none";
+        emailInput.style.borderColor = "";
+    }
 
 })
+
