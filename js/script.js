f2443b6c8628e576fbdf3dd3b9b9b98bd9e0755a
Virginia Anyim <ginaanyim@hotmail.com>
2025-12-30 00:06:26 +0100

    Add updated comments, removed facebook icon and privacy policies in footer

--- js/script.js
+++ js/script.js
@@ -7,14 +7,14 @@ const contactForm = document.getElementById('contactForm');
 const thankYouBox = document.getElementById('thankYouBox');
 
 
-//Lägger till en eventlyssnare för input i meddelandefältet
+//Add event listener for input in message-field 
 messageInput.addEventListener('input', function() {
 
-    //Mäter antal tecken i meddelandefältet
+    //Count numbers of characters 
     const currentLength = messageInput.value.length;
     charCount.textContent = currentLength + " / 20 characters";
 
-    //Ändrar färg på räcknaren baserat på antal tecken
+    //Change color of character count based on length
     if (currentLength >= 20) {
         charCount.style.color = "green";
     } else {
@@ -25,24 +25,24 @@ messageInput.addEventListener('input', function() {
 //Eventlistener for when the form i submitted
 contactForm.addEventListener('submit', function(event){
     event.preventDefault();
+
+    //Reset error messages and styles
+    emailError.style.display = "";
+    emailInput.style.borderColor = "";
     
     //Get values from input fields
     const emailValue = emailInput.value;
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const messageLength = messageInput.value.length;
 
     //Regex-pattern to check valid email format
-    const messageLength = messageInput.value.length;
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     //Check if email is valid and message length is at least 20 characters
     if (!emailPattern.test(emailValue)) {
-        event.preventDefault();
         emailError.textContent = "Please enter a valid email address.";
         emailError.style.display = "block";
         emailInput.style.borderColor = "red";
     } else if (messageLength < 20){
-        event.preventDefault();
-        emailError.style.display = "";
-        emailInput.style.borderColor = "";
         charCount.style.color = "red";
     } else {
         thankYouBox.style.display = "block";
@@ -50,6 +50,7 @@ contactForm.addEventListener('submit', function(event){
 
 })
 
+//Eventlistener for reset button
 const clearBtn = document.getElementById('clearBtn');
 clearBtn.addEventListener('click', function(){
     contactForm.reset();
