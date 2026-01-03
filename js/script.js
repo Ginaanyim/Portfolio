05d8b06b32882db9dcc5a698a2c232246bcf7cbb
Virginia Anyim <ginaanyim@hotmail.com>
2026-01-03 22:50:17 +0100

    Add filter buttons for projects with JS + CSS style

--- js/script.js
+++ js/script.js
@@ -1,3 +1,106 @@
 const lastModified = new Date(document.lastModified);
 const options = { year: 'numeric', month: 'long' };
 document.getElementById('last-update').textContent = lastModified.toLocaleDateString('en-US', options);
+
+//Get form element
+const contactForm = document.getElementById('contactForm');
+
+//Check if form exists on the page (to ensure code only runs on pages with the form)
+if (contactForm) {
+    //Variables for form elements
+    const messageInput = document.getElementById('message');
+    const charCount = document.getElementById('charCount');
+    const emailInput = document.getElementById('email');
+    const emailError = document.getElementById('emailError');
+    const thankYouBox = document.getElementById('thankYouBox');
+
+    //Add event listener for input in message-field 
+    messageInput.addEventListener('input', function() {
+        //Count numbers of characters 
+        const currentLength = messageInput.value.length;
+        charCount.textContent = currentLength + " / 20 characters";
+
+        //Change color of character count based on length
+        if (currentLength >= 20) {
+            charCount.style.color = "green";
+        } else {
+            charCount.style.color = 'black';
+        }
+    });
+
+    //Eventlistener for when the form i submitted
+    contactForm.addEventListener('submit', function(event){
+        event.preventDefault();
+
+        //Reset error messages and styles
+        emailError.style.display = "";
+        emailInput.style.borderColor = "";
+        
+        //Get values from input fields
+        const emailValue = emailInput.value;
+        const messageLength = messageInput.value.length;
+
+        //Regex-pattern to check valid email format
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        //Check if email is valid and message length is at least 20 characters
+        if (!emailPattern.test(emailValue)) {
+            emailError.textContent = "Please enter a valid email address.";
+            emailError.style.display = "block";
+            emailInput.style.borderColor = "red";
+        } else if (messageLength < 20){
+            charCount.style.color = "red";
+        } else {
+            thankYouBox.style.display = "block";
+        }
+    });
+
+    //Eventlistener for reset button
+    const clearBtn = document.getElementById('clearBtn');
+    if (clearBtn) {
+        clearBtn.addEventListener('click', function(){
+            contactForm.reset();
+            charCount.textContent = "0 / 20 characters";
+            charCount.style.color = "black";
+            emailError.style.display = "none";
+            emailInput.style.borderColor = "";
+            thankYouBox.style.display = "none";
+        });
+    }
+}
+
+//Wait until the browser has loaded the HTML content
+document.addEventListener('DOMContentLoaded', function() {
+
+    //Select all filter buttons and project cards
+    const buttons = document.querySelectorAll ('.filter-btn');
+    const cards = document.querySelectorAll ('.project-card');
+
+    //Loop through each button individually 
+    buttons.forEach(function(clickedButton) {
+        //Eventlistener running when a button is clicked
+        clickedButton.addEventListener('click', function(){
+            //Loop through buttons to remove active class
+            buttons.forEach(function(button){
+                button.classList.remove('active');
+            });
+            //Ensures only clicked button is active
+            clickedButton.classList.add('active');
+
+            //Get category namede from clicked button
+            const filterValue = clickedButton.getAttribute('data-filter');
+
+            //Loop through each card to show/hide 
+            cards.forEach(function(card){
+                if (filterValue === 'all' || card.classList.contains(filterValue)) {
+                    card.style.display = 'block';
+                } else {
+                    card.style.display = 'none';
+                }
+            });
+    });
+});
+});
+
+
+
