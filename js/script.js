b28b46849b3ebf8dbf08b4bfc7c55716ebfd7ae2
Virginia Anyim <ginaanyim@hotmail.com>
2026-01-05 02:08:49 +0100

    Add (DOM) and filter logic for projects

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
     //Variables for form elements
     const contactForm = document.getElementById('contactForm');
     const nameInput = document.getElementById('name');
     const messageInput = document.getElementById('message');
     const charCount = document.getElementById('charCount');
     const emailInput = document.getElementById('email');
-    const emailError = document.getElementById('emailError');
     const thankYouBox = document.getElementById('thankYouBox');
 
     function validateName() {
@@ -42,15 +43,17 @@
 
     //Reset all form elements
     function clearForm(){
-        contactForm.reset();
-        charCount.textContent = "0 / 20 characters";
-        charCount.style.color = "black";
-        clearError(nameInput);
-        nameInput.style.borderColor = "";
-        clearError(emailInput);
-        emailInput.style.borderColor = "";
-        clearError(messageInput);
-        messageInput.style.borderColor = "";
+        if (contactForm) {
+            contactForm.reset();
+            charCount.textContent = "0 / 20 characters";
+            charCount.style.color = "black";
+            clearError(nameInput);
+            nameInput.style.borderColor = "";
+            clearError(emailInput);
+            emailInput.style.borderColor = "";
+            clearError(messageInput);
+            messageInput.style.borderColor = "";
+        }
     }
 
     /*Check if form exists on the page (to ensure code only runs on pages with the form)
@@ -67,58 +70,85 @@
             }
         });
 
-    //Eventlistener for when the form i submitted, with validation
-    contactForm.addEventListener('submit', function(event){
-        event.preventDefault();
+        //Eventlistener for when the form i submitted, with validation
+        contactForm.addEventListener('submit', function(event){
+            event.preventDefault();
 
-        let validForm = true;
+            let validForm = true;
 
-        if (!validateName()) {
-            showError(nameInput, "Please enter a valid name.");
-            validForm = false;
-        } else {
-            clearError(nameInput);
-            nameInput.style.borderColor = "green";
-        }
+            if (!validateName()) {
+                showError(nameInput, "Please enter a valid name.");
+                validForm = false;
+            } else {
+                clearError(nameInput);
+                nameInput.style.borderColor = "green";
+            }
 
-        if (!validateEmail()){
-            showError(emailInput, "Please enter a valid email adress.");
-            validForm = false;
-        } else {
-            clearError(emailInput);
-            emailInput.style.borderColor = "green";
-        }
+            if (!validateEmail()){
+                showError(emailInput, "Please enter a valid email adress.");
+                validForm = false;
+            } else {
+                clearError(emailInput);
+                emailInput.style.borderColor = "green";
+            }
 
-        if (!validateMessage()){
-            messageInput.style.borderColor = "#dc3545";
-            charCount.style.color = "#dc3545";            
-            validForm = false;
-        } else {
-            clearError(messageInput);
-            messageInput.style.borderColor = "green";
-        }
+            if (!validateMessage()){
+                messageInput.style.borderColor = "#dc3545";
+                charCount.style.color = "#dc3545";            
+                validForm = false;
+            } else {
+                clearError(messageInput);
+                messageInput.style.borderColor = "green";
+            }
 
-        //Success message if form is valid
-        if (validForm) {
-            const firstName = nameInput.value.split (' ')[0];
-            thankYouBox.textContent = 'Thank you for your message, ' + firstName + '!';
-            thankYouBox.style.display = "block";
+            //Success message if form is valid
+            if (validForm) {
+                const firstName = nameInput.value.split (' ')[0];
+                thankYouBox.textContent = 'Thank you for your message, ' + firstName + '!';
+                thankYouBox.style.display = "block";
 
-            setTimeout(function(){
-                thankYouBox.style.display = "none";
-            }, 3000);
-            clearForm();
-        }
-    });
+                setTimeout(function(){
+                    thankYouBox.style.display = "none";
+                }, 3000);
+                clearForm();
+            }
+        });
 
-    //Eventlistener for reset button
-    const clearBtn = document.getElementById('clearBtn');
+        //Eventlistener for reset button
+        const clearBtn = document.getElementById('clearBtn');
         if (clearBtn) {
-        clearBtn.addEventListener('click', function(){
-            clearForm();
+            clearBtn.addEventListener('click', function(){
+                clearForm();
+            });
+        }
+    }
+
+    //filter logic for projects
+    const buttons = document.querySelectorAll('.filter-btn');
+    const projectContainer = document.getElementById('project-container');
+
+    if (projectContainer) {
+        displayProjects('all');
+
+        buttons.forEach(function(clickedButton) {
+            clickedButton.addEventListener('click', function() {
+
+                buttons.forEach(function(btn) { btn.classList.remove('active'); });
+                clickedButton.classList.add('active');
+
+                const filterValue = clickedButton.getAttribute('data-filter');
+                displayProjects(filterValue);
+            });
         });
     }
-}
+
+    //Function for showing projects with right filter
+    function displayProjects(filterValue) {
+        const container = document.getElementById('project-container');
+        const counter = document.getElementById('project-counter');
+
+        let count = 0;
+}); 
 
 const myProjects = [
     {
@@ -176,57 +206,3 @@ const myProjects = [
         link: "project-6.html",
     }
 ];
-
-function displayProjects(filterValue) {
-    const container = document.getElementById('projectContainer');
-    const counter = document.getElementById('projectCounter');
-
-    let count = 0;
-}
-
-
-
-
-
-
-
-
-
-/*
-
-//Wait until the browser has loaded the HTML content
-document.addEventListener('DOMContentLoaded', function() {
-
-    //Select all filter buttons and project cards
-    const buttons = document.querySelectorAll ('.filter-btn');
-    const cards = document.querySelectorAll ('.project-card');
-
-    //Loop through each button individually 
-    buttons.forEach(function(clickedButton) {
-        //Eventlistener running when a button is clicked
-        clickedButton.addEventListener('click', function(){
-            //Loop through buttons to remove active class
-            buttons.forEach(function(button){
-                button.classList.remove('active');
-            });
-            //Ensures only clicked button is active
-            clickedButton.classList.add('active');
-
-            //Get category namede from clicked button
-            const filterValue = clickedButton.getAttribute('data-filter');
-
-            //Loop through each card to show/hide 
-            cards.forEach(function(card){
-                if (filterValue === 'all' || card.classList.contains(filterValue)) {
-                    card.style.display = 'block';
-                } else {
-                    card.style.display = 'none';
-                }
-            });
-    });
-});
-});
-
-
-
-*/
